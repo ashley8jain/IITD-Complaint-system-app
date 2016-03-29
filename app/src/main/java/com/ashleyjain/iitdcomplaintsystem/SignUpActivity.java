78ddcca3cb4e71504b62f40071ef19f976ab102e
ashleyjain <ashley8jain@gmail.com>
2016-03-29 22:35:17 +0530

    signup api done

--- app/src/main/java/com/ashleyjain/iitdcomplaintsystem/SignUpActivity.java
+++ app/src/main/java/com/ashleyjain/iitdcomplaintsystem/SignUpActivity.java
@@ -12,6 +12,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -21,21 +22,27 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SignUpActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class SignUpActivity extends AppCompatActivity  {
 
-    EditText firstname,lastname,username,department,password;
+    EditText firstname,lastname,username,password;
     Button signup;
-    String hostel;
+    String hostel,dept;
     Context context = SignUpActivity.this;
     Integer typee=1;
+    RadioGroup radioGroup;
 
+    //Radio button handling
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
 
         // Check which radio button was clicked
         switch(view.getId()) {
-            case R.id.admin:
+            case R.id.instituteadmin:
+                if (checked)
+                    typee = 3;
+                break;
+            case R.id.hosteladmin:
                 if (checked)
                     typee = 2;
                 break;
@@ -57,46 +64,85 @@ public class SignUpActivity extends AppCompatActivity implements AdapterView.OnI
 
         Typeface font = Typeface.createFromAsset(getAssets(),"YuppySC-Regular.ttf");
 
-
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         firstname = (EditText) findViewById(R.id.firstname);
         lastname = (EditText) findViewById(R.id.lastname);
         username = (EditText) findViewById(R.id.username);
-        department = (EditText) findViewById(R.id.department);
         password = (EditText) findViewById(R.id.password);
         signup = (Button) findViewById(R.id.sign_up);
-        firstname.setTypeface(font);lastname.setTypeface(font);username.setTypeface(font);department.setTypeface(font);signup.setTypeface(font);
+        firstname.setTypeface(font);lastname.setTypeface(font);username.setTypeface(font);signup.setTypeface(font);
 
         firstname.addTextChangedListener(new checkError(firstname));
         lastname.addTextChangedListener(new checkError(lastname));
         username.addTextChangedListener(new checkError(username));
-        department.addTextChangedListener(new checkError(department));
         password.addTextChangedListener(new checkError(password));
 
         // Spinner element
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerHostel);
-
+        Spinner spinner_hostel = (Spinner) findViewById(R.id.spinnerHostel);
+        Spinner spinner_department = (Spinner) findViewById(R.id.spinnerDepartment);
         // Spinner click listener
-        spinner.setOnItemSelectedListener(this);
+        spinner_hostel.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // On selecting a spinner item
+                hostel = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spinner_department.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // On selecting a spinner item
+                dept = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         // Spinner Drop down elements
-        List<String> categories = new ArrayList<String>();
-        categories.add("Vindhyachal");
-        categories.add("Karakoram");
-        categories.add("Shivalik");
-        categories.add("Khailash");
-        categories.add("Kumaon");
+        List<String> categoriesOfHostel = new ArrayList<String>();
+        categoriesOfHostel.add("Vindhyachal");
+        categoriesOfHostel.add("Karakoram");
+        categoriesOfHostel.add("Shivalik");
+        categoriesOfHostel.add("Khailash");
+        categoriesOfHostel.add("Kumaon");
         // Creating adapter for spinner
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+        ArrayAdapter<String> dataAdapterHostel = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categoriesOfHostel);
 
         // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dataAdapterHostel.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         // attaching data adapter to spinner
-        spinner.setAdapter(dataAdapter);
+        spinner_hostel.setAdapter(dataAdapterHostel);
+
+        List<String> categoriesOfDept = new ArrayList<String>();
+        categoriesOfDept.add("general");
+        categoriesOfDept.add("maintenance");
+        categoriesOfDept.add("mess");
+        categoriesOfDept.add("sports");
+        categoriesOfDept.add("cultural");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapterDepartment = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categoriesOfDept);
+
+        // Drop down layout style - list view with radio button
+        dataAdapterDepartment.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner_department.setAdapter(dataAdapterDepartment);
 
 
 
-        final String[] fn = new String[1];final String[] ln = new String[1]; final String[] un = new String[1]; final String[] dp = new String[1];  final String[] pw = new String[1];
+
+
+        final String[] fn = new String[1];final String[] ln = new String[1]; final String[] un = new String[1]; final String[] pw = new String[1];
 
         //register button
         signup.setOnClickListener(new View.OnClickListener() {
@@ -107,11 +153,11 @@ public class SignUpActivity extends AppCompatActivity implements AdapterView.OnI
                 fn[0] = firstname.getText().toString();
                 ln[0] = lastname.getText().toString();
                 un[0] = username.getText().toString();
-                dp[0] = department.getText().toString();
                 pw[0] = password.getText().toString();
 
                 final ProgressDialog dialog = ProgressDialog.show(context, "", "Signing Up...", true);
-                String url = "http://"+ LoginActivity.ip +"/first/default/register.json?first_name="+fn[0]+"&last_name="+ln[0]+"&hostel_name="+hostel+"&department="+dp[0]+"&type_="+typee+"&password=i"+pw[0]+"&username="+un[0];
+                String url = "http://"+ LoginActivity.ip +"/first/default/register.json?first_name="+fn[0]+"&last_name="+ln[0]+"&hostel_name="+hostel+"&department="+dept+"&type_="+typee+"&password=i"+pw[0]+"&username="+un[0];
+
                 //GET request through stringrequest
                 GETrequest.response(new GETrequest.VolleyCallback() {
                     @Override
@@ -133,7 +179,6 @@ public class SignUpActivity extends AppCompatActivity implements AdapterView.OnI
                     }
                 }, context, url, dialog);
 
-
             }
         });
 
@@ -144,16 +189,4 @@ public class SignUpActivity extends AppCompatActivity implements AdapterView.OnI
 
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        // On selecting a spinner item
-        hostel = parent.getItemAtPosition(position).toString();
-        // Showing selected spinner item
-        //Toast.makeText(parent.getContext(), "Selected: " + department, Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
 }
